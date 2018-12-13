a2e72e81d41557dbfd5b3be267fd6deab33ae2a4
Talib Zaz <musist222@gmail.com>
2018-12-13 12:05:34 +0530

    initial commit

---
+++ fibonacci/fib.py
@@ -0,0 +1,7 @@
+a, b = 0, 1
+
+while a < 10:
+    print(a)
+    a, b = b, a+b
+
+print("The end")
---
+++ hello.py
@@ -0,0 +1,13 @@
+x = 3              # a whole number
+f = 3.1415926      # a floating point number
+name = "Python"    # a string
+
+print(x)
+print(f)
+print(name)
+
+combination = name + " " + name
+print(combination)
+
+sum = f + f
+print(sum)
